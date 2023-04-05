9f6b693d63725aeecda56da57e15c9dd006e84ab
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-05 11:51:45 -0300

    feat(auth): add creating sigunp logic and error validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 // The services classes uses the injectable decorator
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // Generates the password hash
-
+    const hash = await argon.hash(dto.password);
     // Save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
 
-    // return the saved user
-    return { msg: 'I Have signed up' };
+      // Delete user hash to not show it on the returned user
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (err) {
+      // Verify if the error is being catch from the prisma
+      if (err instanceof PrismaClientKnownRequestError) {
+        // This code stands to a duplicated field
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      // If the error does not come from prisma it just need to be thrown
+      throw err;
+    }
   }
 
   signin() {
