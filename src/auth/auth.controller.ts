005ebe15a6366ca244f663215f2cf482570261b7
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-05 10:10:17 -0300

    feat(controller): add authdto and some notes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 // Since it's an auth controller, it's a good practice to put a global prefix route
 @Controller('auth')
@@ -8,8 +9,14 @@ export class AuthController {
 
   // To make a route it's only needed to add an decorator to the type of route
   @Post('signup')
-  signup() {
-    return this.authService.signup();
+  /* 
+    In this way can be declared that a request is expected
+    When working with nestjs it's not a good to use a request object of another library
+    When using the nest decorators it's not necessary to import the request from express...
+    In this way can be declared the expected type of the body 
+  */
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
