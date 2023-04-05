c179408a066f29a88ad2a618b85090d3c3eaba4d
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-05 10:06:36 -0300

    feat(main): add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   // App that's being instantiated
   const app = await NestFactory.create(AppModule);
+  // Enabling the validation pipe to the hole project
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // In this way the validation pipe strips out the element that are not defined at the dto
+      whitelist: true,
+    }),
+  );
   // Port of the server
   await app.listen(3000);
 }
