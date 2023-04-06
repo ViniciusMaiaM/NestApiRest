32f02904d5a49b98bb79419d08af01f865880f44
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-06 16:34:05 -0300

    feat(user): add base user controller and module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards, Req, Patch } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+// With the guard in here, everything that is under it will require the authentication token
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+}
