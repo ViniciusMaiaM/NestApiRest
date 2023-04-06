619dc02fe9d6ffd9c70de3e791f2de2b8d2e66f7
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-04-06 16:35:42 -0300

    feat(get-user.decorator): updat return from get user

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -2,9 +2,13 @@ import { createParamDecorator } from '@nestjs/common';
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContextHost) => {
+  (data: string | undefined, ctx: ExecutionContextHost) => {
     // Declaring that this request is going to be the same as the expres request
     const request: Express.Request = ctx.switchToHttp().getRequest();
+    // In this way it returns the whole user data
+    if (data) {
+      return request.user[data];
+    }
     return request.user;
   },
 );
